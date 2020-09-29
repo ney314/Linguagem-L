9addd335776f7818d42ae80582f2358869de6d35
João Victor <joaovitormunizlopes@gmail.com>
2020-09-29 10:49:52 -0300

    Alterações em alguns condicionais

--- FuncoesSimples.js
+++ FuncoesSimples.js
@@ -6,10 +6,18 @@ import exp from './utils/exp.js';
 import subt from './utils/subt.js';
 import getNumberOfArguments from './utils/getNumberOfArguments.js';
 
-const string = "mult(mult(subt(2,3),soma(3,2)),mult(mult(3,2),sqrt(16)))";
-const array = string.match(/-?[0-9]+|[A-Za-z0-9]+|\S/g);
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const variables  = []; // vetor que ficarão as variáveis declaradas pela linguagem
+
+const funcao = 'function';
+const string = 'string';
+const integer = 'integer'
 
 const FunctionsList = {
   ['sqrt']: [(a)=>{ return sqrt(a)},{args:1}],
@@ -17,7 +25,69 @@ const FunctionsList = {
   ['subt']: [(a,b)=>{return subt(a,b)},{args:2}],
   ['soma']: [(a,b)=>{return soma(a,b)},{args:2}],
   ['divi']: [(a,b)=>{return divi(a,b)},{args:2}],
+  ['exp']:  [(a)=>{return exp(a)},{args:2}]
 };
+
+
+//res = response
+const Tokens ={
+  [funcao]:(argumentos)=>{
+
+    let {pos,arg,args} = argumentos;
+    let position;
+    let res;
+    
+    // criando um novo array com os mesmos elementos de arg
+    let this_arg = array_copy(arg,pos); 
+        
+    let func_name = this_arg.shift(); 
+    
+    /*o primeiro termo seria o nome de uma função  Ex: soma(subt(a,a),b) arg:[ "subt" , "(" , "a", "," , "a", ")", "," , "b" ] onde subt seria o func_name e seria retirado para obter o seu arg em get_argument_line e func_name usado para saber oq executar e pegar as informações da função */
+    pos = pos + get_argument_line(this_arg).length + 3 ;
+
+    /*pegando o  exemplo acima o  get_argument_line de subt seria ["a", "," , "a"] , isso significa que o tamanho dessa função no token é o get_argument_line.length + os itens que foram excluidos que seriam os dois parenteses mais o nome da função [ >"subt"< , "(" , "a", "," , "a", > ")" <] , dessa forma sabemos que a virgula tem que estar no proximo token para obedecer a sintaxe , ou seja , pos = pos(0) + get_argument_line(this_arg).length + 3 . */
+
+    //adiciona o retorno dos valores das funções nested como argumento dessa função
+    args.push(resolve_function(this_arg,func_name)); 
+
+    position=pos;
+
+    res = args;
+   
+    return {position,res}
+  },
+
+  [string]:(argumentos)=>{
+
+    let {pos,args,term} = argumentos;
+
+    let position;
+    let res;
+
+    args.push(term);
+    pos++;
+  
+    position=pos;
+    res = args;
+
+    return {position,res}
+
+  },
+  [integer]:(argumentos)=>{
+      let {pos,args,term} = argumentos;
+
+      let position;
+      let res;
+
+      args.push(term);  
+      pos++;
+
+      position=pos;
+      res = args;
+
+      return {position,res};
+  }
+}
 
 function array_copy(array,from = 0){
   
@@ -32,14 +102,16 @@ function array_copy(array,from = 0){
   return new_array;
 }
 
-
+/*A próxima função verifica se os parenteses estão batendo e retornar o array passado como parametro se estiver certo*/
 /*pego o campo da string que está o argumento , por exemplo no array soma(1,2), o vetor de token seria ["soma","(", "1" , "," , "2", ")" ] , essa função pega o campo ["1" , "," , "2"] que vai ser usado em outra função pra pegar os argumentos separadamente , note que devido ao fato de poder ter nested functions vai haver diversos parenteses e como o parentese é usada para delimitar a string, se faz necessário um contador de parenteses pra verificar a sintaxe e pra pegar o campo corretamente. */
 function get_argument_line(array){
   
+
   if(array[0]=="("){
     let pt_count = 1;
     let iterator = 1;
     let new_array = [];
+
     while(iterator < array.length){
       if(array[iterator] == "("){
         pt_count++;
@@ -47,7 +119,7 @@ function get_argument_line(array){
       if(array[iterator] == ")"){
         pt_count--;
       }
-      if(pt_count < 0 ){
+      if(pt_count < 0  ){
         throw new SyntaxError();
       }
       if(pt_count == 0){
@@ -73,17 +145,17 @@ function get_argument_line(array){
 function identify_token(term){
   
   if(parseInt(term)){
-    return "integer";
+    return integer;
   }
   
   if(term.match(/^(".*")|('.*')$/)){
     
-    return "string";
+    return string;
     
   } // se tiver entre ?apostrofe? ou entre aspas
     
   
- for(let i = 0 ; i<variables.length ; i++){
+  for(let i = 0 ; i<variables.length ; i++){
     if(term == variables[i].name){
       return "variable";
     }
@@ -91,7 +163,7 @@ function identify_token(term){
   
  for(let i = 0 ; i <Object.entries(FunctionsList).length ; i++){
     if(term == Object.entries(FunctionsList)[i][0]){
-      return "function";
+      return funcao;
     }
   } 
   
@@ -103,10 +175,10 @@ function identify_token(term){
 function resolve_function(array,name){
   
 
-  // pega o numero de argumentos da função , devido ao fato de uma função pode ter varios argumentos o numero de argumentos seria pego ao ser declarada contando os itens no detro parentese quando a função é declarada.
+  // pega o numero de argumentos da 1 função função , devido ao fato de uma função pode ter varios argumentos o numero de argumentos seria pego ao ser declarada contando os itens no detro parentese quando a função é declarada.
   let n_args = getNumberOfArguments(FunctionsList,name); 
 
-   //espaço que vai ser armazenado os argumentos da função já "numerizados".
+  //espaço que vai ser armazenado os argumentos da função já "numerizados".
   let args=[];                         
   let arg = get_argument_line(array); 
 
@@ -120,56 +192,18 @@ function resolve_function(array,name){
   for(let i = 0 ; i < n_args; i++){   
     
     // numa função soma(a,b) -> arg = ["a", "," , "b"] não se espera uma virgula antes do primeiro token , como em soma(,a,b) => ["," , "a", "," , "b"], portanto na primeira iteração isso vai ser sempre verdadeiro (i=0) sendo coeso com a sintaxe soma (a,b) ou soma(a,b,c) e assim por diante...
-    if(arg[pos-1]=="," | i== 0){  
-      
-    }
-    else{
+    if(!(arg[pos-1]=="," | i== 0)){  
       throw new SyntaxError();
     }
 
      //Se ao declarar uma função for necessario informar a natureza dos argumentos , como em C => blabla(int a , string b) , seria necessario criar um outro campo em função com a natureza dos argumentos e tambem necessário fazer uma estrutura de fluxo nesses cases abaixo.
-    switch (identify_token(term)){  
+    let token = identify_token(term); 
+    let response = Tokens[token]({arg,pos,args,term})
 
-      case "function":
-          
-        // criando um novo array com os mesmos elementos de arg
-        let this_arg = array_copy(arg,pos); 
-        let func_name = this_arg.shift(); 
-        
-        /*o primeiro termo seria o nome de uma função  Ex: soma(subt(a,a),b) arg:[ "subt" , "(" , "a", "," , "a", ")", "," , "b" ] onde subt seria o func_name e seria retirado para obter o seu arg em get_argument_line e func_name usado para saber oq executar e pegar as informações da função */
-        pos = pos + get_argument_line(this_arg).length + 3 ;
-        
-        /*pegando o  exemplo acima o  get_argument_line de subt seria ["a", "," , "a"] , isso significa que o tamanho dessa função no token é o get_argument_line.length + os itens que foram excluidos que seriam os dois parenteses mais o nome da função [ >"subt"< , "(" , "a", "," , "a", > ")" <] , dessa forma sabemos que a virgula tem que estar no proximo token para obedecer a sintaxe , ou seja , pos = pos(0) + get_argument_line(this_arg).length + 3 . */
-        
-        //adiciona o retorno dos valores das funções nested como argumento dessa função
-        args.push(resolve_function(this_arg,func_name));    
+    let {position,res} = response;
 
-        
-        break;
-
-      case "integer":
-        
-        args.push(parseInt(term));
-        pos++;
-        break;
-        
-      case "string":
-        
-          args.push(term);
-          pos++
-          break;
-        
-      case "variable":
-        
-          args.push(term.value);  
-          pos++;
-          break;
-
-      default:
-        throw new SyntaxError("string não encontrada na posição " + pos);
-        //string não reconhecida
-      }
-    pos++;
+    args=res;
+    pos=position+1;
     term = arg[pos]
    
   }
@@ -182,7 +216,15 @@ function resolve_function(array,name){
   }
   
   return FunctionsList[name][0](...args);
-
 } 
-let name = array.shift();
-console.log(resolve_function(array,name));
+
+rl.question("Insira a sentença ? ",(sentence) => {
+  const array = sentence.match(/-?[0-9]+|[A-Za-z0-9]+|\S/g);
+  const name = array.shift();
+
+  console.log(resolve_function(array,name));  
+});
+
+rl.on("close", function() {
+  process.exit(0);
+});
